40f6382962e67e5ef4214bf4b1db55586906be34
Sebastian Waisbrot <seppo0010@gmail.com>
2015-08-23 00:12:15 -0700

    Documentation

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['stal'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='stal/enum.Set.html' title='stal::Set'>Set</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
